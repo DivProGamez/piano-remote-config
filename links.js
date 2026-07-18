6ca9d8a0c0a2a4de098200365bcb0b2d27b6b5ae
DivProGamez <ouadi.noureddine.ma@gmail.com>
2026-07-18 01:17:17 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,1 +1,27 @@
+window.REMOTE_LINKS = {
+  version: 2,
 
+  // صفحة التطبيق داخل Amazon Appstore
+  appDetails:
+    'amzn://apps/android?asin=B0G2ZLSLSH',
+
+  // جميع تطبيقات نفس حساب المطور
+  moreApps:
+    'amzn://apps/android?asin=B0G2ZLSLSH&showAll=1',
+
+  // Amazon ما عندهاش Deep Link منفصل مباشر للتقييم
+  // لذلك كيتفتح Detail Page ديال التطبيق
+  rate:
+    'amzn://apps/android?asin=B0G2ZLSLSH',
+
+  // صفحة التحديث، مهمة خصوصاً لـFire TV
+  appUpdate:
+    'amzn://apps/android?asin=B0G2ZLSLSH&intent=app_update',
+
+  // روابط احتياطية للمتصفح
+  appDetailsWeb:
+    'https://www.amazon.com/dp/B0G2ZLSLSH',
+
+  moreAppsWeb:
+    'https://www.amazon.com/gp/mas/dl/android?asin=B0G2ZLSLSH&showAll=1'
+};
